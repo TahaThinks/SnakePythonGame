75b4e12ea95fd45a4283180581171cf2d910305a
TahaThinks <tahaa4587@gmail.com>
2024-02-14 16:45:07 +0400

    Refine Code

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ screen.setup(600,600)
 screen.bgcolor("black")
 screen.title("Taha SnakeGame")
 
-snake = Turtle("square")
-snake.penup()
-snake.color("white")
-snake.shapesize(1, 3, 1)
+starting_positions = [(0,0),(-20,0),(-40,0)]
 
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
