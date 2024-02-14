736e6f2ae84e229bea15b857a5e6f9b2b1b71bd1
TahaThinks <tahaa4587@gmail.com>
2024-02-14 16:26:19 +0400

    Set Screen size, color and title

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import turtle
 from turtle import Screen, Turtle
 
-arrow = Turtle()
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("Taha SnakeGame")
+
 snake = Turtle("square")
+snake.color("white")
 snake.shapesize(1,3,1)
 
 
 
-screen = Screen()
+
 screen.exitonclick()
