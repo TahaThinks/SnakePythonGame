6b77b0c28b6699d960b30d8c863009d9f41ed5bc
TahaThinks <tahaa4587@gmail.com>
2024-02-19 12:12:08 +0400

    Refine Code

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from turtle import Screen, Turtle
 import time
 
 screen = Screen()
-screen.setup(600,600)
+screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Taha SnakeGame")
 screen.tracer(0)
 
-starting_positions = [(0,0),(-20,0),(-40,0)]
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+# Create a Snake Instance
 segments = []
-
+# Move the Snake across the screen
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
@@ -25,14 +26,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.5)
-
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
+    # Automatic Movement of the Snake
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
         segments[seg_num].goto(new_x, new_y)
     segments[0].forward(20)
     segments[0].left(90)
-
-
 
 screen.exitonclick()
