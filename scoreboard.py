2580e2592a54758f414181e065dbd86afc828a13
TahaThinks <tahaa4587@gmail.com>
2024-02-20 16:22:44 +0400

    Print Game Over when touches the screen

--- scoreboard.py
+++ scoreboard.py
@@ -15,8 +15,11 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
-
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
